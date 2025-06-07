9a9d93b16d158a6e1f9f7d3273241103dad25173
sshivanshg <sshivanshg@gmail.com>
2025-06-07 14:13:15 +0530

    refactor: separate backend and clean project root

--- client/src/components/proposals/elements/index.ts
+++
@@ -1,5 +0,0 @@
-export { TextElement } from './TextElement';
-export { HeadingElement } from './HeadingElement';
-export { ImageElement } from './ImageElement';
-export { PricingTableElement } from './PricingTableElement';
-export { ScopeBlockElement } from './ScopeBlockElement';
